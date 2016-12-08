ee2715c4a74734560a356674734f7f0ce7ffb7e8
uda-desigh-program <uda_desigh_program@zouyu.com>
2016-12-08 20:09:43 +0800

    add two quiz solution in best_hand.py

---
+++ best_hand.py
@@ -0,0 +1,76 @@
+# CS 212, hw1-1: 7-card stud
+#
+# -----------------
+# User Instructions
+#
+# Write a function best_hand(hand) that takes a seven
+# card hand as input and returns the best possible 5
+# card hand. The itertools library has some functions
+# that may help you solve this problem.
+#
+# Write a function best_wild_hand(hand) that takes as
+# input a 7-card hand and returns the best 5 card hand.
+# In this problem, it is possible for a hand to include
+# jokers. Jokers will be treated as 'wild cards' which
+# can take any rank or suit of the same color. The
+# black joker, '?B', can be used as any spade or club
+# and the red joker, '?R', can be used as any heart
+# or diamond.
+#
+# The itertools library may be helpful. Feel free to
+# define multiple functions if it helps you solve the
+# problem.
+#
+# -----------------
+# Grading Notes
+#
+# Muliple correct answers will be accepted in cases
+# where the best hand is ambiguous (for example, if
+# you have 4 kings and 3 queens, there are three best
+# hands: 4 kings along with any of the three queens).
+
+import itertools
+from poker import hand_rank, hand_rank3
+
+
+def best_hand(hand):
+    "From a 7-card hand, return the best 5 card hand."
+    #all_hands = itertools.combinations(hand, 5)
+    return max(itertools.combinations(hand, 5), key=hand_rank)
+
+def best_wild_hand(hand):
+    "Try all values for jokers in all 5-card selections."
+    hands = set(
+        best_hand(h) for h in itertools.product(*map(replacements, hand))
+    )
+    return max(hands, key=hand_rank)
+
+blackcards = [r+s for r in '23456789TJQKA' for s in 'SC']
+redcards = [r+s for r in '23456789TJQKA' for s in 'HD']
+
+def replacements(card):
+    """Return a list of possible cards"""
+    if card == '?B': return blackcards
+    elif card == '?R': return redcards
+    else: return [card]
+
+def test_best_hand():
+    assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split()))
+            == ['6C', '7C', '8C', '9C', 'TC'])
+    assert (sorted(best_hand("TD TC TH 7C 7D 8C 8S".split()))
+            == ['8C', '8S', 'TC', 'TD', 'TH'])
+    assert (sorted(best_hand("JD TC TH 7C 7D 7S 7H".split()))
+            == ['7C', '7D', '7H', '7S', 'JD'])
+    return 'test_best_hand passes'
+
+def test_best_wild_hand():
+    assert (sorted(best_wild_hand("6C 7C 8C 9C TC 5C ?B".split()))
+            == ['7C', '8C', '9C', 'JC', 'TC'])
+    assert (sorted(best_wild_hand("TD TC 5H 5C 7C ?R ?B".split()))
+            == ['7C', 'TC', 'TD', 'TH', 'TS'])
+    assert (sorted(best_wild_hand("JD TC TH 7C 7D 7S 7H".split()))
+            == ['7C', '7D', '7H', '7S', 'JD'])
+    return 'test_best_wild_hand passes'
+
+print test_best_hand()
+print test_best_wild_hand()
