912908772c3164a664695176451907a37d4a6126
uda-desigh-program <uda_desigh_program@zouyu.com>
2017-01-04 01:21:07 +0800

    Decorator for n_ary a function .......

--- Lesson3/n_ary.py
+++ Lesson3/n_ary.py
@@ -1,3 +1,20 @@
+from functools import update_wrapper
+
+def decorator(d):
+    "Make function d a decorator: d wraps a function fn."
+    def _d(fn):
+        return update_wrapper(d(fn), fn)
+    update_wrapper(_d, d)
+    return _d
+
+"""
+# also work
+def decorator(d):
+    return lambda fn: update_wrapper(d(fn), fn)
+decorator = decorator(decorator)
+"""
+
+@decorator
 def n_ary(f):
     """Given binary function f(x, y), return an n_ary function such
     that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
@@ -5,11 +22,24 @@ def n_ary(f):
         return x if not args else f(x, n_ary_f(*args))
     return n_ary_f
 
+@n_ary
 def add(x, y):
     return x + y
 
+@n_ary
+def mul(x, y):
+    return x*y
+
 #print add(1, 2)
-t = n_ary(add)
-#print t(1)
-#print t(1,2)
-print t(1,2,3)
+print add(1)
+print add(1,2)
+print add(1,2,3)
+print add(1,2,3,4)
+
+
+print mul(1)
+print mul(1,2)
+print mul(1,2,3)
+print mul(1,2,3,4)
+
+print help(add)
