0249bfd6f39d518e345af69c26f576ef38694d84
uda-desigh-program <uda_desigh_program@zouyu.com>
2017-01-07 23:50:01 +0800

    lowest cost search codes for generalised codes

--- Lesson4/bridge_problem.py
+++ Lesson4/bridge_problem.py
@@ -112,8 +112,42 @@ def bsuccessors2(state):
         )
 
 def bridge_problem2(here):
-    #TODO more efficient solution only take (here, there) as the state
-    pass
+    Fail = []
+    here = frozenset(here) | frozenset(['light'])
+    explored = set()  # set of states we have visited
+    # State will be a (peoplelight_here, peoplelight_there) tuple
+    # E.g. ({1, 2, 5, 10, 'light'}, {})
+    frontier = [[(here, frozenset())]]  # ordered list of paths we have blazed
+    while frontier:
+        path = frontier.pop(0)
+        here1, there1 = state1 = path[-1]
+        if not here1 or (len(here1) == 1 and 'light' in here1):
+            return path
+        explored.add(state1)
+        pcost = path_cost(path)
+        for (state, action) in bsuccessors2(state1).items():
+            if state not in explored:
+                total_cost = pcost + bcost(action)
+                path2 = path + [(action, total_cost), state]
+                add_to_frontier(frontier, path2)
+    return Fail
+
+def add_to_frontier(frontier, path):
+    "Add path to frontier, replacing costlier path if there is one."
+    # (This could be done more efficiently.)
+    # Find if there is an old path to the final state of this path.
+    old = None
+    for i,p in enumerate(frontier):
+        if p[-1] == path[-1]:
+            old = i
+            break
+    if old is not None and path_cost(frontier[old]) < path_cost(path):
+        return # Old path was better; do nothing
+    elif old is not None:
+        del frontier[old] # Old path was worse; delete it
+    ## Now add the new path and re-sort
+    frontier.append(path)
+    frontier.sort(key=path_cost)
 
 def path_cost(path):
     """The total cost of a path (which is stored in a tuple
@@ -122,7 +156,7 @@ def path_cost(path):
     if len(path) < 3:
         return 0
     else:
-        return path[-2][-1]
+        return path[-2][-1] # total_cost
 
 def bcost(action):
     """Returns the cost (a number) of an action in the
