8813c581b5d2b3608babdff5e7818da5c34976a3
uda-desigh-program <uda_desigh_program@zouyu.com>
2017-01-20 13:55:54 +0800

    1. use namedtuple 2. add 2 strategies max_win & max_diffs

--- Lesson5/play_pig.py
+++ Lesson5/play_pig.py
@@ -7,16 +7,17 @@
 # you:     an int, the other player's current score.
 # pending: an int, the number of points accumulated on current turn, not yet scored
 import random
+from collections import namedtuple
 
 other = (1, 0)
-goal = 50
+goal = 40
 possible_moves = ['roll', 'hold']
+State = namedtuple('State', 'p me you pending')
 
 def hold(state):
     """Apply the hold action to a state to yield a new state:
     Reap the 'pending' points and it becomes the other player's turn."""
-    p, me, you, pending = state
-    return (other[p], you, me+pending, 0)
+    return State(other[state.p], state.you, state.me+state.pending, 0)
 
 
 def roll(state, d):
@@ -24,11 +25,10 @@ def roll(state, d):
     If d is 1, get 1 point (losing any accumulated 'pending' points),
     and it is the other player's turn. If d > 1, add d to 'pending' points."""
     assert 1 <= d <= 6
-    p, me, you, pending = state
     if d == 1:
-        return (other[p], you, me+1, 0)
+        return State(other[state.p], state.you, state.me+1, 0)
     else:
-        return (p, me, you, pending+d)
+        return State(state.p, state.me, state.you, state.pending+d)
 
 def dierolls():
     """Generate die rolls."""
@@ -41,16 +41,21 @@ def play_pig(A, B, dierolls=dierolls()):
     which must be 'hold' or 'roll', and we update the state accordingly.
     When one player's score exceeds the goal, return that player."""
     strategies = [A, B]
-    (p, me, you, pending) = state = (0, 0, 0, 0)
+    state = State(0, 0, 0, 0)
     while True:
-        if me >= goal:
-            return strategies[p]
-        elif you >= goal:
-            return strategies[other[p]]
-        elif strategies[p](state) == 'hold':
-            (p, me, you, pending) = state = hold(state)
-        else: #'roll'
-            (p, me, you, pending) = state = roll(state, next(dierolls))
+        if state.me >= goal:
+            return strategies[state.p]
+        elif state.you >= goal:
+            return strategies[other[state.p]]
+        else:
+            action = strategies[state.p](state)
+            if action == 'hold':
+                state = hold(state)
+            elif action == 'roll':
+                state = roll(state, next(dierolls))
+            else: # illegal action
+                return strategies[other[state.p]]
+
 
 ############ STRATEGIES ################
 
@@ -78,10 +83,105 @@ def hold_at(x):
 
 ############ STRATEGIES ################
 
+
+############ OPTIMAL PIG ################
+
+def Q_pig(state, action, U_pig):
+    """The expected value of choosing action in state."""
+    if action == 'hold':
+        return 1 - U_pig(hold(state))
+    elif action == 'roll':
+        return (1 - U_pig(roll(state, 1)) + sum(U_pig(roll(state, d)) for d in (2,3,4,5,6))) / 6.0
+    raise ValueError
+
+def pig_actions(state):
+    """the legal actions from a state"""
+    return ['roll', 'hold'] if state.pending else ['roll']
+
+from Lesson3.memo import memo
+
+@memo
+def Pwin(state):
+    """The utility of a state; here just the probability that an opltimal player
+     whose turn it is to move can  win from current state."""
+    if state.me+state.pending >= goal:
+        return 1.0
+    elif state.you >= goal:
+        return 0.0
+    else:
+        return max(Q_pig(state, action, Pwin)
+                   for action in pig_actions(state))
+
+
+def best_action(state, actions, Q, U):
+    "Return the optimal action for a state, given U."
+    def EU(action): return Q(state, action, U)
+    return max(actions(state), key=EU)
+
+#the max win probability Strategy
+def max_wins(state):
+    "The optimal pig strategy chooses an action with the highest win probability."
+    s = State(*state)
+    return best_action(s, pig_actions, Q_pig, Pwin)
+
+######### MAX DIFFERENTIAL PIG #############
+@memo
+def win_diff(state):
+    "The utility of a state: here the winning differential (pos or neg)."
+    if state.me + state.pending >= goal or state.you >= goal:
+        return (state.me + state.pending - state.you)
+    else:
+        return max(Q_pig(state, action, win_diff)
+                   for action in pig_actions(state))
+
+#the max differential Strategy
+def max_diffs(state):
+    """A strategy that maximizes the expected difference between my final score
+    and my opponent's."""
+    s = State(*state)
+    return best_action(s, pig_actions, Q_pig, win_diff)
+
+
+
 def test():
-    A, B = hold_at(50), clueless
-    rolls = iter([6,6,6,6,6,6,6,6,6])
-    assert play_pig(A, B, rolls) == A
-    return 'test passes'
+    assert(max_wins((1, 5, 34, 4)))   == "roll"
+    assert(max_wins((1, 18, 27, 8)))  == "roll"
+    assert(max_wins((0, 23, 8, 8)))   == "roll"
+    assert(max_wins((0, 31, 22, 9)))  == "hold"
+    assert(max_wins((1, 11, 13, 21))) == "roll"
+    assert(max_wins((1, 33, 16, 6)))  == "roll"
+    assert(max_wins((1, 12, 17, 27))) == "roll"
+    assert(max_wins((1, 9, 32, 5)))   == "roll"
+    assert(max_wins((0, 28, 27, 5)))  == "roll"
+    assert(max_wins((1, 7, 26, 34)))  == "hold"
+    assert(max_wins((1, 20, 29, 17))) == "roll"
+    assert(max_wins((0, 34, 23, 7)))  == "hold"
+    assert(max_wins((0, 30, 23, 11))) == "hold"
+    assert(max_wins((0, 22, 36, 6)))  == "roll"
+    assert(max_wins((0, 21, 38, 12))) == "roll"
+    assert(max_wins((0, 1, 13, 21)))  == "roll"
+    assert(max_wins((0, 11, 25, 14))) == "roll"
+    assert(max_wins((0, 22, 4, 7)))   == "roll"
+    assert(max_wins((1, 28, 3, 2)))   == "roll"
+    assert(max_wins((0, 11, 0, 24)))  == "roll"
+
+    # The first three test cases are examples where max_wins and
+    # max_diffs return the same action.
+    assert (max_diffs((1, 26, 21, 15))) == "hold"
+    assert (max_diffs((1, 23, 36, 7))) == "roll"
+    assert (max_diffs((0, 29, 4, 3))) == "roll"
+    # The remaining test cases are examples where max_wins and
+    # max_diffs return different actions.
+    assert (max_diffs((0, 36, 32, 5))) == "roll"
+    assert (max_diffs((1, 37, 16, 3))) == "roll"
+    assert (max_diffs((1, 33, 39, 7))) == "roll"
+    assert (max_diffs((0, 7, 9, 18))) == "hold"
+    assert (max_diffs((1, 0, 35, 35))) == "hold"
+    assert (max_diffs((0, 36, 7, 4))) == "roll"
+    assert (max_diffs((1, 5, 12, 21))) == "hold"
+    assert (max_diffs((0, 3, 13, 27))) == "hold"
+    assert (max_diffs((0, 0, 39, 37))) == "hold"
+
+    return 'tests pass'
 
 print test()
